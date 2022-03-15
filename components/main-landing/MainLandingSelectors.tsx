dea0194554dce36f35b40206378f4c501dc7b66f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-15 23:33:08 +0100

    Styles WIP

--- components/main-landing/MainLandingSelectors.tsx
+++ components/main-landing/MainLandingSelectors.tsx
@@ -8,63 +8,66 @@ const MainLandingSelectors: React.FC = () => {
 
   return (
     <div className={styles["main-landing-selectors-container"]}>
-      {/* Country */}
+      <h3>Select a spot</h3>
       <div>
-        <label htmlFor="country">Country:</label>
-        <select name="country" id="country" ref={countrySelect}>
-          <option selected disabled>
-            Country
-          </option>
-          {["Spain", "Other country"].map((country) => {
-            return (
-              <option value={country.toLowerCase().replaceAll(" ", "_")}>
-                {country}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      {/* Region */}
-      <div>
-        <label htmlFor="region">Region:</label>
-        <select
-          name="region"
-          id="region"
-          ref={regionSelect}
-          disabled={countrySelect.current !== null}
-        >
-          <option selected disabled>
-            Region
-          </option>
-          {["Cantabria", "Euskadi"].map((region) => {
-            return (
-              <option value={region.toLowerCase().replaceAll(" ", "_")}>
-                {region}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      {/* Spot */}
-      <div>
-        <label htmlFor="spot">Spot:</label>
-        <select
-          name="spot"
-          id="spot"
-          ref={spotSelect}
-          disabled={regionSelect.current !== null}
-        >
-          <option selected disabled>
-            Spot
-          </option>
-          {["La Arena", "Sopelana"].map((spot) => {
-            return (
-              <option value={spot.toLowerCase().replaceAll(" ", "_")}>
-                {spot}
-              </option>
-            );
-          })}
-        </select>
+        {/* Country */}
+        <div>
+          <label htmlFor="country">Country:</label>
+          <select name="country" id="country" ref={countrySelect}>
+            <option selected disabled>
+              Country
+            </option>
+            {["Spain", "Other country"].map((country) => {
+              return (
+                <option value={country.toLowerCase().replaceAll(" ", "_")}>
+                  {country}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        {/* Region */}
+        <div>
+          <label htmlFor="region">Region:</label>
+          <select
+            name="region"
+            id="region"
+            ref={regionSelect}
+            disabled={countrySelect.current !== null}
+          >
+            <option selected disabled>
+              Region
+            </option>
+            {["Cantabria", "Euskadi"].map((region) => {
+              return (
+                <option value={region.toLowerCase().replaceAll(" ", "_")}>
+                  {region}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        {/* Spot */}
+        <div>
+          <label htmlFor="spot">Spot:</label>
+          <select
+            name="spot"
+            id="spot"
+            ref={spotSelect}
+            disabled={regionSelect.current !== null}
+          >
+            <option selected disabled>
+              Spot
+            </option>
+            {["La Arena", "Sopelana"].map((spot) => {
+              return (
+                <option value={spot.toLowerCase().replaceAll(" ", "_")}>
+                  {spot}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
     </div>
   );
