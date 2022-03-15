0d512c461202f0f809caaa2847457d510022cc2a
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-15 23:11:42 +0100

    Adding SurfForecast widget

--- pages/spots/[spotId].tsx
+++ pages/spots/[spotId].tsx
@@ -5,7 +5,42 @@ import { useEffect } from "react";
 const SurfSpotPage: NextPage = () => {
   const router = useRouter();
 
-  return <>This is the page for spot {router.query.spotId}</>;
+  return (
+    <>
+      This is the page for spot {router.query.spotId}
+      <link
+        href="//www.surf-forecast.com/stylesheets/widget.css"
+        media="screen"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <div className="wf-width-cont surf-fc-widget">
+        <div className="widget-container">
+          <div className="external-cont">
+            <iframe
+              className="surf-fc-i"
+              allowTransparency={true}
+              src="//www.surf-forecast.com/breaks/Playade-Ereaga/forecasts/widget/a"
+              scrolling="no"
+              frameBorder={0}
+              marginWidth={0}
+              marginHeight={0}
+            ></iframe>
+            <div className="footer">
+              <a className="logo" href="//www.surf-forecast.com/">
+                <img
+                  src="//www.surf-forecast.com/images/widget.png"
+                  alt="Widget"
+                  width="1"
+                  height="1"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
