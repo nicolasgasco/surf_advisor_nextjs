1f98de762024d3f5eaa08b85e8d2c34d20a55b46
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-15 16:50:06 +0100

    Styled footer

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,5 +1,17 @@
+import styles from "./Footer.module.scss";
+
 const Footer: React.FC = () => {
-  return <footer>Made with ♡ by Nicolas Gasco</footer>;
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://nicolasgasco.com/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Made with ♡ by Nicolas Gasco
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
