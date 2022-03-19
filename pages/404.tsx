d54abdf1a424ba24354745837c82af6a50155fb8
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-19 23:55:13 +0100

    404 Page WIP

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 const NotFoundPage = () => {
-  return <h1>404 custom page</h1>;
+  return (
+    <main>
+      <h1>Page wasn't found :(</h1>
+      <Image src="/svg/404.svg" width={400} height={300} layout="responsive" />
+    </main>
+  );
 };
 
 export default NotFoundPage;
