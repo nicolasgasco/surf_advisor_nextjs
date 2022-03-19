df374e4443f6b26296c33927222c80f7d5a1ec1d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-20 00:42:31 +0100

    Fixing lint errors

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const NotFoundPage = () => {
   return (
     <main>
-      <h1>Page wasn't found :(</h1>
-      <Image src="/svg/404.svg" width={400} height={300} layout="responsive" />
+      <h1>Page wasn&apos;t found :(</h1>
+      <Image src="/svg/404.svg" width={400} height={300} layout="responsive" alt=""/>
     </main>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,9 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=UnifrakturCook:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <PageLayout>
-        <Component {...pageProps} />
-      </PageLayout>
-    </>
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
   );
 }
 
