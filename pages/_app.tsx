6c417e360c4d9541732bbbd3c7a5b6ab8667f7f6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-20 00:21:55 +0100

    Changed font imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import PageLayout from "../components/ui/PageLayout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=UnifrakturCook:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </>
   );
 }
 
